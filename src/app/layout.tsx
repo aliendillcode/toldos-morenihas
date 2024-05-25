845717dbb410cc38168ec570d9b29461477d5e98
sgtdill <sgtdill@gmail.com>
2024-05-25 12:51:00 -0400

    First commit after next install

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Toldos Moreninha",
+  description: "A mais de 10 anos Atendendo Campo Grande e Região",
+  authors: [{ name: "Alienderson Santos" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
